b3d2db8666e7976c2f70c6a68b1d7012d1010ea9
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-05-07 23:13:10 +0300

    added get* functions set

--- contracts/order_manager.sol
+++ contracts/order_manager.sol
@@ -1,27 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 contract OrderManager {
-    enum OrderStatus {
-        processing,
-        complited,
-        canceled
-    }
+    uint8 private processing = 1;
+    uint8 private complited  = 1 << 1;
+    uint8 private canceled   = 1 << 2;
 
     struct Order {
         uint orderDate;
         uint32 productId;
         uint32 productCount;
         uint8 status;
-        byte[32] ipfs_hash;
+        bytes1[32] ipfs_hash;
     }
 
     uint private orderId = 0;
     mapping(uint => Order) private orderes;
 
-    event newOrder(uint orderId);
+    event newOrderCreated(uint orderId);
 
-    function creadeOrder(uint32 _productId, uint32 _productCount, byte[32] memory _ipfs_hash)
+    function creadeOrder(uint32 _productId, uint32 _productCount, bytes1[32] memory _ipfs_hash)
         external
         payable
     {
@@ -29,13 +27,32 @@ contract OrderManager {
         newOrder.orderDate = block.timestamp;
         newOrder.productId = _productId;
         newOrder.productCount = _productCount;
-        newOrder.status = OrderStatus.processing;
+        newOrder.status = processing;
         newOrder.ipfs_hash = _ipfs_hash;
-        emit newOrder(orderId);
+        emit newOrderCreated(orderId);
         orderId++;
     }
 
-    function getOrderById(uint orderId) external view returns(Order) {
-        return orderes[orderId];
+    function getOrderById(uint ID) external view returns(Order memory) {
+        return orderes[ID];
+    }
+
+    function _getOrderesByFilter(uint8 filter) private view returns(Order[] memory) {
+        Order[] memory result = new Order[](orderId);
+        uint counter = 0;
+        for (uint i = 0; i < orderId; i++) {
+            if (orderes[i].status & filter != 0) {
+                result[counter++] = orderes[i];
+            }
+        }
+        return result;
+    }
+
+    function getOrderesByFilter(uint8 filter) external view returns(Order[] memory) {
+        return _getOrderesByFilter(filter);
+    }
+
+    function getOrderesList() external view returns(Order[] memory) {
+        return _getOrderesByFilter(processing | complited | canceled);
     }
 }
